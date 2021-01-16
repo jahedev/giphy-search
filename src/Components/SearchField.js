dee45c344737b9a89c8646945a99ed10a5c80811
Thierno Souleymane Diallo <thiernosouleymane24@gmail.com>
2021-01-16 01:51:57 -0500

    finish the project, by dividing files into seperate components and adding some filters

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -5,12 +5,14 @@ import  './Grid.css'
 import GifGard from './GifGard'
 
 
+
 class Search extends Component{
     constructor(props){
         super(props)
         this.state ={
             gifs: [],
-            inputvalue: 'mom'
+            inputvalue: ' ',
+            numberGif:1
         }
 
         this.changeHandler = this.changeHandler.bind(this)
@@ -24,14 +26,26 @@ class Search extends Component{
         )
     }
 
-    async componentDidMount() {
-        axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.inputvalue}&api_key=sc71PXHt3BEJg208Ct4xBM6K5OTbJ4BT`)
-        .then((res) => {
-          this.setState({ gifs: res.data.data });
-        });
-    }
-    render(){
+    
+
+
+    async componentDidMount(arg) {
         
+     
+        axios.get(arg)
+        .then((res) => {
+            let arr = []
+            if(!Array.isArray(res.data.data)){arr.push(res.data.data); this.setState({ gifs: arr }) }
+            else this.setState({ gifs: res.data.data });
+        });  
+        
+
+    }
+
+   
+
+    render(){
+
         return(
 
             <div>
@@ -39,32 +53,34 @@ class Search extends Component{
                   <input name="inputvalue" type="text" placeholder=" type your search" value={this.state.inputvalue} onChange={this.changeHandler} id="searchInput"/>
                    
                   
-                  <button onClick={()=>this.componentDidMount()} id="searchButton"> Search </button>
+                  <button onClick={()=>this.componentDidMount(`http://api.giphy.com/v1/gifs/search?q=${this.state.inputvalue.trim()}&api_key=sc71PXHt3BEJg208Ct4xBM6K5OTbJ4BT`)} id="searchButton"> Search </button>
               <div id="filter">
-                <div id="filterbutton">
-                    <button id="button1"> Random </button>
-                    <button id="button2">Tranding</button>
-                </div>
+                  <div id="filterbutton">
+                    <button id="button1" onClick={()=>this.componentDidMount(`http://api.giphy.com/v1/gifs/random?api_key=sc71PXHt3BEJg208Ct4xBM6K5OTbJ4BT`)}> Random </button>
+                    <button id="button2" onClick={()=>this.componentDidMount(`http://api.giphy.com/v1/gifs/trending?api_key=sc71PXHt3BEJg208Ct4xBM6K5OTbJ4BT`)}>Tranding</button>
+                     
+                  </div>
 
                   <div id="increament">
                        Number Of Gifs
-                      <button > + </button>
-                      <button > - </button>
-                  </div>
+                      <button onClick={()=>{if(this.state.numberGif < this.state.gifs.length) this.setState({numberGif: this.state.numberGif +1})}}> + </button>
+                      <button   onClick={()=>{if(this.state.numberGif >0) this.setState({numberGif: this.state.numberGif - 1})}}> - </button>
+                   </div>
 
-            </div>
+               </div>
 
                  
                 </div>
 
-                <div class="grid-container">  
-                  {
-                     this.state.gifs.map((item, index) => 
+                <div class="grid-container">
+                                        
+                    {                     
+                        this.state.gifs.map((item, index) => 
 
-                         <div> <GifGard key={index} src={item.images['downsized'].url} title={item.title}/> </div>
-             
-                    ) 
-                  } 
+                           (index < this.state.numberGif) ? <div>  <GifGard key={index} src={item.images['original'].url} title={item.title}/> </div> : ''
+                        ) 
+
+                    }
                 </div>  
             </div>
            
